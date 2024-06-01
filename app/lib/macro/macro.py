fea3244de9c597bc38a860579e43f77976a03efe
Anagarm4747 <anagram4747@gmail.com>
2024-06-02 00:06:16 +0900

    マクロ部分を別ファイルへ移動

---
+++ app/lib/macro/macro.py
@@ -0,0 +1,56 @@
+"""
+このモジュールは、マクロ操作を定義する関数を提供します
+"""
+
+import time
+# 座標ずれ対策で、pyautoguiを追加している
+import pyautogui  # pylint: disable=<W0611>
+import pydirectinput
+
+
+def consume_prime():
+    """
+        おまかせ選択で琥珀を選択し、錬成を行う
+
+        Args:
+            None
+        Returns:
+            None
+        """
+    pydirectinput.press('x')
+    precise_sleep(0.017)  # 17msの遅延
+    pydirectinput.press('space')
+    precise_sleep(0.034)  # 34msの遅延
+    pydirectinput.press('space')
+
+
+def skip():
+    """
+    傀異錬成の結果を受け取らずにスキップする
+
+    Args:
+        None
+    Returns:
+        None
+    """
+    pydirectinput.press('b')
+    precise_sleep(0.034)  # 17msの遅延
+    pydirectinput.press('a')
+    precise_sleep(0.017)  # 17msの遅延
+    pydirectinput.press('space')
+    precise_sleep(0.017)  # 17msの遅延
+    pydirectinput.press('space')
+
+
+def precise_sleep(duration):
+    """
+    指定された期間だけスリープする
+
+    Args:
+        duration (float): スリープする時間（秒）
+    Returns:
+        None
+    """
+    end_time = time.time() + duration
+    while time.time() < end_time:
+        pass
