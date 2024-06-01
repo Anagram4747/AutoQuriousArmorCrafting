b2a543eca92e23caedf39b7b78192f0765d452c7
Anagarm4747 <anagram4747@gmail.com>
2024-06-01 10:58:33 +0900

    画像認識部分完成したっぽい？

--- app/vision/vision_api.py
+++ app/vision/vision_api.py
@@ -3,9 +3,9 @@
 """
 
 import io
+import cv2  # pylint: disable=<E0401>
 from google.cloud import vision
 from google.api_core.exceptions import GoogleAPICallError, InvalidArgument
-import numpy as np
 
 
 def extract_text_from_image(image_path):
@@ -25,7 +25,8 @@ def extract_text_from_image(image_path):
             content = image_file.read()
 
         image = vision.Image(content=content)
-        response = client.text_detection(image=image)
+        response = client.text_detection(  # pylint: disable=<E1101>
+            image=image)
 
         if response.error.message:
             raise GoogleAPICallError(response.error.message)
@@ -35,35 +36,6 @@ def extract_text_from_image(image_path):
 
     except (GoogleAPICallError, InvalidArgument) as e:
         raise RuntimeError(f'Error during text extraction: {str(e)}') from e
-
-
-def extract_image_features(image_path):
-    """
-    画像ファイルから特徴量を抽出する関数。
-
-    Args:
-        image_path (str): 画像ファイルのパス。
-
-    Returns:
-        list: 画像の特徴量ベクトル。
-    """
-    client = vision.ImageAnnotatorClient()
-
-    try:
-        with io.open(image_path, 'rb') as image_file:
-            content = image_file.read()
-
-        image = vision.Image(content=content)
-        response = client.feature_extraction(image=image)
-
-        if response.error.message:
-            raise GoogleAPICallError(response.error.message)
-
-        features = response.feature_extraction
-        return features
-
-    except (GoogleAPICallError, InvalidArgument) as e:
-        raise RuntimeError(f'Error during feature extraction: {str(e)}') from e
 
 
 def compare_images(image_path1, image_path2):
@@ -77,11 +49,9 @@ def compare_images(image_path1, image_path2):
     Returns:
         float: 2つの画像間の類似度スコア。
     """
-    features1 = extract_image_features(image_path1)
-    features2 = extract_image_features(image_path2)
+    img1 = cv2.imread(image_path1, cv2.IMREAD_COLOR)
+    img2 = cv2.imread(image_path2, cv2.IMREAD_COLOR)
 
-    # 特徴量ベクトルのユークリッド距離を計算
-    distance = np.linalg.norm(np.array(features1) - np.array(features2))
-    # 類似度スコアは距離の逆数とする（距離が小さいほど類似度が高い）
-    similarity = 1 / (1 + distance)
-    return similarity
+    mssim, _ = cv2.quality.QualitySSIM_compute(img1, img2)
+
+    return (mssim[0] + mssim[1] + mssim[2]) / 3
